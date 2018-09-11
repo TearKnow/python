7228ecd7e18bec2484b9e35ddc62cfd0dfaf4855
jack-hp <904916790@qq.com>
2018-09-11 22:52:22 +0800

    no liner regression

---
+++ tensorflow/base/pyplot.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_data = np.linspace(-1, 1, 200)[:, np.newaxis]
+prediction_value = np.linspace(-0.1, 0.1, 200)[:, np.newaxis]
+
+plt.figure()
+plt.plot(x_data, prediction_value, 'r-', lw = 5)#x×ø±ê£¬y×ø±ê
+plt.show()
